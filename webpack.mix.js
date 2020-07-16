c017a80bfd11f54617e7ec115d6c0f3fecba1f9d
Ali A. Dhillon <aleedhx@gmail.com>
2020-07-16 09:59:28 +0500

    installs tailwindcss preset

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+   .postCss('resources/css/app.css', 'public/css', [
+      require('tailwindcss'),
+   ]);
+
+if (mix.inProduction()) mix.version();
+
