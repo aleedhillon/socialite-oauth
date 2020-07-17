7e6eed05cb05777f216fc5142e25228ee32aa6ff
Ali A. Dhillon <aleedhx@gmail.com>
2020-07-17 12:38:03 +0500

    Adds login with google button on login screen

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,8 +53,11 @@
                 </button>
             </div>
             <div class="mt-6">
-                <a href="{{ route('oauth.redirect') }}" type="submit" class="w-1/2 py-2 px-4 text-center bg-black rounded-md text-white text-sm focus:outline-none">
+                <a href="{{ route('oauth.redirect', 'github') }}" type="submit" class="block py-2 px-4 text-center bg-black hover:bg-gray-800 rounded-md text-white text-sm focus:outline-none">
                     Login with <span class="font-bold">Github</span>
+                </a>
+                <a href="{{ route('oauth.redirect', 'google') }}" type="submit" class="block mt-2 py-2 px-4 text-center bg-green-700 hover:bg-green-500 rounded-md text-white text-sm focus:outline-none">
+                    Login with <span class="font-bold">Google</span>
                 </a>
             </div>
         </form>
