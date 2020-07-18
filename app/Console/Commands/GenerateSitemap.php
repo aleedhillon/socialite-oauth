43a78b46d2c4f9ffc1874588ebcac32750c75d1f
Ali A. Dhillon <aleedhx@gmail.com>
2020-07-18 08:13:40 +0500

    creates sitemap:generate command

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Sitemap\SitemapGenerator;
+
+class GenerateSitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sitemap:generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This will generate a sitemap for the website.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        SitemapGenerator::create(config('app.url'))
+                        ->getSitemap()
+                        // ->writeToDisk('local', 'sitemaps/sitemap.xml');
+                        ->writeToFile(public_path('sitemap.xml'));
+        
+        $this->info('Sitemap has been creatad.');
+    }
+}
